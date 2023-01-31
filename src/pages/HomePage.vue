3642d1d6d3d906edab477e086f9b2afede0e79bb
CarolinaInglethe <carolinainglethe@gmail.com>
2023-01-31 01:58:55 -0300

    Update Home page ,Styles and Assets

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,21 @@
 <template>
-    <div>
-        {{ msg }}
+    <div class="home-page">
+        <div class="elements-row">
+          <section>
+            <div class="home-page-texts">
+              <h4>Olá pessoas,</h4>
+              <h1>Eu sou uma <br/> Programadora</h1>
+              <h4>Seja bem-vindo(a) ao meu Portfólio website</h4>
+            </div>
+            <button>Saiba mais sobre mim</button>
+          </section>
+          <img class="homepage-image-developer-girl" src="../assets/imagem-programadora.png.png" alt="Imagem de uma mulher no computador">
+        </div>
+        <div class="homepage-social-media">
+          <img src="../assets/logo-github.png" alt="logo da ferramenta git">
+          <img src="../assets/logo-linkedin.png" alt="logo da ferramenta linkedin">
+          <img src="../assets/logo-instagram.png" alt="logo da ferramenta instagram">
+        </div>
     </div>
 </template>
 
@@ -14,4 +29,29 @@ export default {
 </script>
 
 <style>
+ .home-page {
+  padding: 100px 55px;
+ }
+ .home-page-texts h1 {
+  font-size: xxx-large;
+ }
+ .home-page button {
+  background-color: rgba(66, 45, 94, 0.959);
+  border: 0px;
+  border-radius: 6px;
+  color: rgb(243, 246, 248);
+  font-size: large;
+  margin-top: 40px;
+  padding: 10px 16px;
+ }
+ .homepage-image-developer-girl {
+  width: 450px;
+ }
+ .homepage-social-media {
+  margin-top: 50px;
+ }
+ .homepage-social-media img {
+  margin: 0px 15px;
+  width: 40px;
+ }
 </style>
