034c324edd892ca2f3577034cacb9483c298c403
CarolinaInglethe <carolinainglethe@gmail.com>
2023-01-31 16:35:39 -0300

    Update, add vue router and restructure files

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <HeaderComponent/>
-  <HomePage msg="Welcome to my portfolio"/>
+  <HeaderComponent />
 </template>
 
 <script>
-import HomePage from './pages/HomePage.vue';
-import HeaderComponent from './components/HeaderComponent.vue'
+import HeaderComponent from './components/HeaderComponent.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HeaderComponent,
-    HomePage
-  }
+ export default {
+    name: "App",
+    components: { HeaderComponent }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,18 +1,19 @@
 <template>
-    <div class="header-component">
-        <div class="header-elements">
-          <img src="../../public/img/image-logo-html-girl-purple.png" alt="" width="67">
+  <div class="header-component">
+      <div class="header-elements">
+          <img src="../../public/img/image-logo-html-girl-purple.png" alt="">
           <h2>Carolina Inglethe</h2>
-        </div>
-        <div class="header-elements nav-bar">
-          <p>Home</p>
-          <p>Sobre</p>
-          <p>Portfólio</p>
-          <p>Tecnologias</p>
-          <p>Experiência</p>
-          <p>Contato</p>
-        </div>
-    </div>
+      </div>
+      <nav class="header-elements nav-bar">
+          <router-link class="link-navbar" to="/">Home</router-link>
+          <router-link class="link-navbar" to="/about">Sobre</router-link>
+          <router-link class="link-navbar" to="/portfolio">Portfólio</router-link>
+          <router-link class="link-navbar" to="/tecnologias">Tecnologias</router-link>
+          <router-link class="link-navbar" to="/experiencias">Experiências</router-link>
+          <router-link class="link-navbar" to="/contato">Contato</router-link>
+      </nav>
+  </div>
+  <router-view/>
 </template>
 
 <script>
@@ -25,14 +26,18 @@
   .header-component {
     display: flex;
     justify-content: space-around;
+    padding: 2%;
   }
   .header-elements {
     display: flex;
   }
   .header-elements img {
     margin: 0px 12px;
+    width: 67px;
   }
-  .header-elements p {
-    padding: 0px 17px;
+  .link-navbar {
+    color: black;
+    padding: 3% 20px;
+    text-decoration: none;
   }
 </style>
