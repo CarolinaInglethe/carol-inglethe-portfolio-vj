50d8412afdd5b1c0ee7d2c1094a171280c3426cc
CarolinaInglethe <carolinainglethe@gmail.com>
2023-01-31 17:32:52 -0300

    Updates, Home page , Links for social medias

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -20,6 +20,20 @@
  export default {
     name: 'HeaderComponent'
  }
+
+ let currentState = history.state;
+ console.log(currentState)
+ 
+ switch(currentState) {
+  case '/':
+    console.log('Esta na pagina inicial');
+    break;
+  case '/sobre':
+    console.log('Está na pagina sobre');
+    break;
+  default:
+    console.log('Nenhuma')
+}
 </script>
 
 <style>
