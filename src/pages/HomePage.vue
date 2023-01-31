72d4c3a1db9cd239be1005fc5b48ea9b25671b20
CarolinaInglethe <carolinainglethe@gmail.com>
2023-01-30 22:51:32 -0300

    Init Project, create Home Page

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        {{ msg }}
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style>
+</style>
